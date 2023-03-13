e14edf30d526788673fdcabad570d2567d6abbfe
marti <martinagarciaa012@gmail.com>
2023-03-13 13:17:23 -0300

    cambios

--- src/main/java/com/springproyect/WebConfig.java
+++ src/main/java/com/springproyect/WebConfig.java
@@ -25,7 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
 
     }
 
